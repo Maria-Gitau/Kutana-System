ca9eca4187147664bf63ee68693de2b3608a74d3
Maria-Gitau <mariahgitau@gmail.com>
2021-08-11 10:56:30 +0300

    initial commit

---
+++ app/src/main/java/com/example/kutanasystem/Model/SalesMan.java
@@ -0,0 +1,10 @@
+package com.example.kutanasystem.Model;
+
+public class SalesMan {
+    public String  salary , img;
+
+    public SalesMan(String salary, String img) {
+        this.salary = salary;
+        this.img = img;
+    }
+}
